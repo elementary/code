9dc511224d7b91a3991bca00016b48a2920beee0
Mario Guerriero <mefrio.g@gmail.com>
2012-05-25 16:56:31 +0200

    fixed bug #1004294

--- src/config.vala.cmake
+++ src/config.vala.cmake
@@ -6,5 +6,10 @@ public const string RELEASE_NAME = "@RELEASE_NAME@";
 public const string VERSION = "@VERSION@";
 public const string VERSION_INFO = "@VERSION_INFO@";
 public const string PLUGINDIR = "@PLUGINDIR@";
+/* Translation template for .desktop file */
+public const string COMMENT = N_("Edit text files");
+public const string GENERIC = N_("Text Editor");
+public const string NEW_DOCUMENT = N_("Create a new document");
+public const string NEW_WINDOW = N_("Create a new window");
 }
  
