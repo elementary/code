73adfe12abf21b5660da0d66cf089f31055de3a0
ttosttos@gmail.com <>
2012-05-30 02:52:19 -0700

    Adds quicklist consistency

--- src/config.vala.cmake
+++ src/config.vala.cmake
@@ -1,15 +1,18 @@
 namespace Constants {
-public const string DATADIR = "@DATADIR@";
-public const string PKGDATADIR = "@PKGDATADIR@";
-public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
-public const string RELEASE_NAME = "@RELEASE_NAME@";
-public const string VERSION = "@VERSION@";
-public const string VERSION_INFO = "@VERSION_INFO@";
-public const string PLUGINDIR = "@PLUGINDIR@";
-/* Translation template for .desktop file */
-public const string COMMENT = N_("Edit text files");
-public const string GENERIC = N_("Text Editor");
-public const string NEW_DOCUMENT = N_("Create a new document");
-public const string NEW_WINDOW = N_("Create a new window");
+   public const string DATADIR = "@DATADIR@";
+   public const string PKGDATADIR = "@PKGDATADIR@";
+   public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
+   public const string RELEASE_NAME = "@RELEASE_NAME@";
+   public const string VERSION = "@VERSION@";
+   public const string VERSION_INFO = "@VERSION_INFO@";
+   public const string PLUGINDIR = "@PLUGINDIR@";
+
+   /* Translatable launcher (.desktop) strings to be added to   */
+   /* template (.pot) file. These strings should reflect any    */
+   /* changes in these launcher keys in .desktop file.          */
+   public const string COMMENT = N_("Edit text files");
+   public const string GENERIC = N_("Text Editor");
+   public const string NEW_DOCUMENT = N_("New Window");
+   public const string NEW_WINDOW = N_("New Window");
 }
  
