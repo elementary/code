aca8d19c6ec2b6efde1ce0978cd6d0d69387b478
Mario Guerriero <mefrio.g@gmail.com>
2013-05-25 09:32:13 +0200

    fixed lib generation

--- src/scratchcore.pc.cmake
+++ src/scratchcore.pc.cmake
@@ -1,12 +1,12 @@
 prefix=@PREFIX@
 exec_prefix=@DOLLAR@{prefix}
 libdir=@DOLLAR@{prefix}/lib
-includedir=@DOLLAR@{prefix}/include/
+includedir=@DOLLAR@{prefix}/include
  
 Name: Scratch
 Description: Scratch headers  
 Version: 0.1  
-Libs: -lscratchcore
+Libs: -L@DOLLAR@{libdir} -lscratchcore
 Cflags: -I@DOLLAR@{includedir}/scratch
 Requires: gtk+-3.0 gee-1.0 granite libsoup-2.4
 
