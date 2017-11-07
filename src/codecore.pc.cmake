7986dc473a8fc9b0e702607d886738f8d3f6f0c9
Cody Garver <cody@elementary.io>
2017-11-07 10:23:31 -0600

    Rename scratchcore to codecore

--- src/codecore.pc.cmake
+++ src/codecore.pc.cmake
@@ -3,10 +3,10 @@ exec_prefix=@DOLLAR@{prefix}
 libdir=@DOLLAR@{prefix}/lib
 includedir=@DOLLAR@{prefix}/include
  
-Name: Scratch
-Description: Scratch headers  
+Name: Code
+Description: Code headers  
 Version: 0.1  
-Libs: -L@DOLLAR@{libdir} -lscratchcore
-Cflags: -I@DOLLAR@{includedir}/scratch
+Libs: -L@DOLLAR@{libdir} -l@LIBNAME@
+Cflags: -I@DOLLAR@{includedir}/@CMAKE_PROJECT_NAME@
 Requires: gtk+-3.0 gee-0.8 granite libsoup-2.4
 
